9bfda7bb9326923d8541ebfc478825e43f3f7287
trubi <trubi@email.cz>
2021-08-11 16:51:22 +0200

    Revert "Remove modules"
    This reverts commit f8547c5f4d76b2a0c08e44f07083ce11d06e9fd5.

---
+++ terraform-datadog-sqs/main.tf
@@ -0,0 +1,29 @@
+
+resource "datadog_monitor" "sqs_number_of_messages_monitor" {
+  name = "${var.service_name_readable} – SQS - Number of messages"
+  count = var.env == "production" ? 1 : 0
+  locked = true
+  tags = var.tags
+  type = "metric alert"
+  message = var.message
+  query = "max(last_5m):avg:aws.sqs.approximate_number_of_messages_visible{queuename:${lower(var.queue_name)},env:${var.env}} > ${var.queue_messages_critical}"
+  monitor_thresholds {
+    warning = var.queue_messages_warning
+    critical = var.queue_messages_critical
+  }
+}
+
+resource "datadog_monitor" "sqs_number_of_messages_dead_letter_monitor" {
+  name = "${var.service_name_readable} – SQS - Number of messages in dead letter"
+  count = var.env == "production" ? 1 : 0
+  locked = true
+  tags = var.tags
+  type = "metric alert"
+  message = var.message
+  query = "max(last_5m):avg:aws.sqs.approximate_number_of_messages_visible{queuename:${lower(var.dead_letter_queue_name)},${var.env}} > ${var.dead_letter_queue_messages_critical}"
+  monitor_thresholds {
+    warning = var.dead_letter_queue_messages_warning
+    critical = var.dead_letter_queue_messages_critical
+  }
+  renotify_interval = var.dead_letter_queue_renotify_interval
+}
