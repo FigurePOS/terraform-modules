b848f821ff687ba905877c2599c7c7b9f6362f75
trubi <trubi@email.cz>
2021-08-12 10:15:38 +0200

    terraform-datadog-slo-express-endpoint

---
+++ terraform-datadog-slo-express-endpoint/variables.tf
@@ -0,0 +1,54 @@
+
+variable "env" {
+  type = string
+}
+
+variable "service" {
+  type = string
+}
+
+variable "resource_name" {
+  type = string
+}
+
+variable "service_name_readable" {
+  type = string
+}
+
+variable "resource_name_readable" {
+  type = string
+}
+
+variable "interval" {
+  type = string
+}
+
+variable "error_rate_target" {
+  type = number
+}
+
+variable "latency_percentile" {
+  type = string
+}
+
+variable "latency_target" {
+  type = number
+}
+
+variable "slo_error_rate_target" {
+  type = number
+}
+
+variable "slo_latency_target" {
+  type = number
+}
+
+variable "tags" {
+  type = list(string)
+  default = []
+}
+
+variable "message" {
+  type = string
+  default = "@slack-figure-alerts"
+}
