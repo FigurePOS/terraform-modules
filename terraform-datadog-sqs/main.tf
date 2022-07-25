ab92ecdded8e6912d6d8fddef71de2e312111d26
trubi <trubi@email.cz>
2022-07-25 18:27:40 +0200

    Change "at least once" to "at all times" for sqs queue monitoring

--- terraform-datadog-sqs/main.tf
+++ terraform-datadog-sqs/main.tf
@@ -6,7 +6,7 @@ resource "datadog_monitor" "sqs_number_of_messages_monitor" {
   tags = var.tags
   type = "metric alert"
   message = var.message
-  query = "max(last_5m):avg:aws.sqs.approximate_number_of_messages_visible{queuename:${lower(var.queue_name)},env:${var.env}} > ${var.queue_messages_critical}"
+  query = "min(last_5m):avg:aws.sqs.approximate_number_of_messages_visible{queuename:${lower(var.queue_name)},env:${var.env}} > ${var.queue_messages_critical}"
   monitor_thresholds {
     warning = var.queue_messages_warning
     critical = var.queue_messages_critical
@@ -20,7 +20,7 @@ resource "datadog_monitor" "sqs_number_of_messages_dead_letter_monitor" {
   tags = var.tags
   type = "metric alert"
   message = var.message
-  query = "max(last_5m):avg:aws.sqs.approximate_number_of_messages_visible{queuename:${lower(var.dead_letter_queue_name)},env:${var.env}} > ${var.dead_letter_queue_messages_critical}"
+  query = "min(last_5m):avg:aws.sqs.approximate_number_of_messages_visible{queuename:${lower(var.dead_letter_queue_name)},env:${var.env}} > ${var.dead_letter_queue_messages_critical}"
   monitor_thresholds {
     warning = var.dead_letter_queue_messages_warning
     critical = var.dead_letter_queue_messages_critical
