4c4b47140dbbb9c76d4cccf7a7179e9b5e72489c
trubi <trubi@email.cz>
2023-04-18 10:30:42 +0200

    feat: module for universal monitor-based SLO

---
+++ terraform-datadog-metric-slo/variables.tf
@@ -0,0 +1,39 @@
+
+variable "tags" {
+  type = list(string)
+  default = []
+}
+
+variable "monitor_name" {
+  type = string
+}
+
+variable "monitor_message" {
+  type = string
+}
+
+variable "monitor_eval_interval" {
+  type = string
+  default = "last_5m"
+}
+
+variable "monitor_eval_fn" {
+  type = string
+  default = "max"
+}
+
+variable "monitor_query" {
+  type = string
+}
+
+variable "monitor_threshold" {
+  type = number
+}
+
+variable "slo_name" {
+  type = string
+}
+
+variable "slo_thresholds" {
+  type = list(object({ timeframe: string, target: number }))
+}
