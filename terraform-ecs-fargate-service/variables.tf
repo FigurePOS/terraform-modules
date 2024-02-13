e0e64ae831151e3b46e205a04c30e972fedff0da
Jakub Matějka <215660+JakubMatejka@users.noreply.github.com>
2024-02-13 15:18:00 +0100

    feat: deploy service using terraform FGR3-2215 (#18)
    * feat: feat: deploy service using terraform
    
    * remove checkov
    
    * fix codestyle

--- terraform-ecs-fargate-service/variables.tf
+++ terraform-ecs-fargate-service/variables.tf
@@ -1,58 +1,122 @@
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "deployment_tag" {
+  type = string
+}
+
+variable "desired_count" {
+  type    = number
+  default = 1
+}
+
+variable "ecr_repository_url" {
+  type = string
+}
+
+variable "ecs_cluster_id" {
+  type = string
+}
+
+variable "entry_point" {
+  type    = string
+  default = "build/index.js"
+}
 
 variable "env" {
   type = string
 }
-variable "ecs_cluster_id" {
+
+variable "git_commit_hash" {
   type = string
 }
-variable "vpc_id" {
+
+variable "git_repository" {
   type = string
 }
-variable "service_port" {
-  type = number
+
+variable "lb_health_check_healthy_threshold" {
+  type    = number
+  default = 3
 }
+
+variable "lb_health_check_path" {
+  type = string
+}
+
+variable "lb_health_check_unhealthy_threshold" {
+  type    = number
+  default = 2
+}
+
+variable "lb_listener_rule_host_header" {
+  type = map(list(string))
+  default = {
+    development = ["api2-dev.figurepos.com", "dev.figureapi.dev"]
+    production  = ["api2.figurepos.com", "prod.figureapi.dev"]
+  }
+}
+
+variable "lb_listener_rule_path_pattern" {
+  type = list(string)
+}
+
+variable "security_group_ids" {
+  type = list(string)
+}
+
+variable "service_custom_definition" {
+  type    = map(any)
+  default = {}
+}
+
+variable "service_envs" {
+  type    = list(any)
+  default = []
+}
+
 variable "service_name" {
   type = string
 }
+
+variable "service_port" {
+  type    = number
+  default = 4000
+}
+
+variable "service_secrets" {
+  type    = list(any)
+  default = []
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
+
+variable "task_cpu" {
+  type    = number
+  default = 256
+}
+
+variable "task_memory" {
+  type    = number
+  default = 512
+}
+
 variable "task_execution_role_arn" {
   type = string
 }
 variable "task_role_arn" {
   type = string
 }
-variable "load_balancer_listener_arn" {
-  type = string
-}
-variable "load_balancer_listener_rule_host_headers" {
-  type = list(string)
-}
-variable "load_balancer_listener_rule_path_patterns" {
-  type = list(string)
-}
-variable "load_balancer_health_check_path" {
+
+variable "vpc_id" {
   type = string
 }
 
-variable "load_balancer_health_check_healthy_threshold" {
-  type    = number
-  default = 5
-}
 
-variable "load_balancer_health_check_unhealthy_threshold" {
-  type    = number
-  default = 2
-}
-
-variable "ecr_repository_url" {
-  type = string
-}
-variable "desired_count" {
-  type    = number
-  default = 1
-}
-variable "subnet_ids" {
-  type = list(string)
-}
-variable "security_group_ids" {
-  type = list(string)
+locals {
+  lb_listener_rule_host_header = var.lb_listener_rule_host_header[var.env]
 }
