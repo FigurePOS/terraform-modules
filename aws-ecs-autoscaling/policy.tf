a0835847f9815f3c4f867fefd5f28894277a0ba6
Krcma <49247463+Krcma@users.noreply.github.com>
2024-11-26 14:31:30 +0100

    feat: add memory target tracking to aws-ecs-autoscaling FGR3-5365 (#55)

--- aws-ecs-autoscaling/policy.tf
+++ aws-ecs-autoscaling/policy.tf
@@ -6,7 +6,7 @@ resource "aws_appautoscaling_target" "this" {
   service_namespace  = "ecs"
 }
 
-resource "aws_appautoscaling_policy" "target_tracking" {
+resource "aws_appautoscaling_policy" "cpu_target_tracking" {
   name               = "${var.ecs_service_name}-cpu-target-tracking"
   policy_type        = "TargetTrackingScaling"
   resource_id        = aws_appautoscaling_target.this.resource_id
@@ -18,6 +18,25 @@ resource "aws_appautoscaling_policy" "target_tracking" {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
     target_value       = var.cpu_target_value
+    scale_in_cooldown  = var.scale_in_cooldown
+    scale_out_cooldown = var.scale_out_cooldown
+  }
+}
+
+resource "aws_appautoscaling_policy" "memory_target_tracking" {
+  count = var.memory_target_value != null ? 1 : 0
+
+  name               = "${var.ecs_service_name}-memory-target-tracking"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.this.resource_id
+  scalable_dimension = aws_appautoscaling_target.this.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.this.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "MemoryUtilization"
+    }
+    target_value       = var.memory_target_value
     scale_in_cooldown  = var.scale_in_cooldown
     scale_out_cooldown = var.scale_out_cooldown
   }
