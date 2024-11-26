d53e9059e82c8fafa936f39be071406d8810786a
krcma <krcma.jan@seznam.cz>
2024-11-26 14:26:30 +0100

    feat: add output FGR3-5365

--- aws-ecs-autoscaling/outputs.tf
+++ aws-ecs-autoscaling/outputs.tf
@@ -1,4 +1,9 @@
-output "scaling_policy_arn" {
-  description = "The ARN of the target tracking scaling policy"
-  value       = aws_appautoscaling_policy.target_tracking.arn
+output "cpu_scaling_policy_arn" {
+  description = "The ARN of the cpu target tracking scaling policy"
+  value       = aws_appautoscaling_policy.cpu_target_tracking.arn
+}
+
+output "memory_scaling_policy_arn" {
+  description = "The ARN of the memory target tracking scaling policy"
+  value       = aws_appautoscaling_policy.memory_target_tracking.arn
 }
