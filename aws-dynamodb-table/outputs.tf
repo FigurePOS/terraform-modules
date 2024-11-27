753455eea4d1668b26a586b9358692be2dd8e539
krcma <krcma.jan@seznam.cz>
2024-11-27 17:00:19 +0100

    feat: add stream_arn output to dynamodb table FGR3-5372

--- aws-dynamodb-table/outputs.tf
+++ aws-dynamodb-table/outputs.tf
@@ -5,3 +5,7 @@ output "dynamodb_table_name" {
 output "dynamodb_table_arn" {
   value = aws_dynamodb_table.this.arn
 }
+
+output "dynamodb_table_stream_arn" {
+  value = aws_dynamodb_table.this.stream_arn
+}
