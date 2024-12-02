d01e37ddf2eb55c4e5168e036592cf569bfe4054
Krcma <49247463+Krcma@users.noreply.github.com>
2024-12-02 16:38:27 +0100

    feat: add domain name to s3 bucket outputs FGR3-5314 (#61)

--- aws-s3-bucket/outputs.tf
+++ aws-s3-bucket/outputs.tf
@@ -6,7 +6,10 @@ output "bucket_arn" {
   value = aws_s3_bucket.bucket.arn
 }
 
+output "bucket_domain_name" {
+  value = aws_s3_bucket.bucket.bucket_domain_name
+}
+
 output "bucket_website_endpoint" {
   value = aws_s3_bucket_website_configuration.website_configuration[*].website_endpoint
 }
-
