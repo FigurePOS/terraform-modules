c9070675aa414c8269909abb207e3ba1fc2ee4d0
Jakub Matejka <215660+JakubMatejka@users.noreply.github.com>
2025-04-02 17:47:35 +0200

    increase log retention to 7 days

--- aws-ecs-service/cloudwatch.tf
+++ aws-ecs-service/cloudwatch.tf
@@ -2,5 +2,5 @@ resource "aws_cloudwatch_log_group" "log_group" {
   # checkov:skip=CKV_AWS_158:Log group is not encrypted. TODO
   # checkov:skip=CKV_AWS_338:Ensure CloudWatch log groups retains logs for at least 1 year.
   name              = "/figure/container/${var.service_name}"
-  retention_in_days = 3
+  retention_in_days = 7
 }
