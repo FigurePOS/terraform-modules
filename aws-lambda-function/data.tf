5b791d5220e4ac0ba54590cf840acdc9173ab4ee
Jakub Matějka <215660+JakubMatejka@users.noreply.github.com>
2025-06-24 16:43:54 +0200

    fix(aws-lambda-function): enable dd tracing FGR3-7035 (#78)

--- aws-lambda-function/data.tf
+++ aws-lambda-function/data.tf
@@ -6,6 +6,6 @@ data "aws_s3_bucket" "lambda_deployment" {
   bucket = "fgr-lambda-deployment-${var.env}"
 } 
 
-data "aws_ssm_parameter" "datadog_api_key" {
+data "aws_secretsmanager_secret" "datadog_api_key" {
   name = "/lambda/datadog/api_key"
 }
