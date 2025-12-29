2ae9e0640bb82f3e8fc0d4097101e1430dcad46f
Jakub Matějka <215660+JakubMatejka@users.noreply.github.com>
2025-12-29 10:49:07 +0100

    feat: create alarms for lambda functions FGR3-8385 (#101)

--- aws-sqs-queue/data.tf
+++ aws-sqs-queue/data.tf
@@ -2,9 +2,9 @@ data "aws_kms_key" "sqs_encryption_key" {
   key_id = "alias/sqs-encryption-key"
 }
 
-data "aws_sns_topic" "chatbot_slack" {
+data "aws_sns_topic" "alerts_slack" {
   count = local.cloudwatch_count
-  name  = "cloudwatch-sqs-alarms-to-slack"
+  name  = "alerts-to-slack"
 }
 
 data "aws_sns_topic" "rootly_oncall" {
